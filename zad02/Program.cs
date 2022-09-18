2a207b419ce20561092a9ff4e55555700b361bd0
max <matrixbyine.gmail.com>
2022-09-18 22:21:29 +0300

    Сумма чисел от M до N

---
+++ zad02/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumber(int M, int N)
+{
+   if (M == N) return M;
+   return N + SumNumber(M, N - 1);
+}
+Console.WriteLine($"Сумма чисел = {SumNumber(1, 15)}");
+
